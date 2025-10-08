b9120e5549ca35535fe26430837347b2c5122dfd
Serhii Shvets <wert2all@gmail.com>
2025-10-08 10:47:11 +0300

    feat: add LSP support

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -1,7 +1,25 @@
 local L = {
   installed = {
-    lua  = {},
-    bash = {},
+    lua = {
+      lsp = {
+        lua_ls = {
+          filetypes = {'lua'},
+          settings = {
+            Lua = {
+              workspace = {
+                library = {
+                  vim.fn.expand "$VIMRUNTIME/lua",
+                  vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
+                },
+              },
+            },
+          },
+        }
+      },
+    },
+    bash = {
+      lsp = {bashls = {}}
+    },
   }
 }
 
@@ -11,6 +29,27 @@ function L.treesitter()
     table.insert(l, lang)
   end
   return l
+end
+
+function L.lsp()
+  local l = {}
+  for lang, opts in pairs(L.installed) do
+    if opts.lsp then
+      for name, config in pairs(opts.lsp) do
+        l[name] = config
+      end
+    end
+  end
+  return l;
+end
+
+function L.mason()
+  local servers = L.lsp()
+  local m = {}
+  for name, cfg in pairs(servers) do
+    table.insert(m, name)
+  end
+  return m
 end
 
 return L
