eeed160b7442a0cd2621e0eb29524610fe5704cc
Serhii Shvets <wert2all@gmail.com>
2025-10-09 20:52:29 +0300

    fix: remove useless configs

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -45,12 +45,4 @@ return {
     treesitter = { "markdown", "markdown_inline" },
     mason = { "marksman", "prettier" },
   },
-
-  installed = {
-    lua = require("config.languages.lua"),
-    bash = require("config.languages.bash"),
-    markdown = require("config.languages.markdown"),
-    markdown_inline = {},
-    yaml = require("config.languages.yaml"),
-  },
 }
