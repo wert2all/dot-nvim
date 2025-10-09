ea3930ce56d6be78045505d0f3e85385f9cf2478
Serhii Shvets <wert2all@gmail.com>
2025-10-09 15:30:12 +0300

    refactor: refactor yaml language

--- lua/config/languages.lua
+++ lua/config/languages.lua
@@ -6,31 +6,7 @@ local L = {
     bash            = require("config.languages.bash"),
     markdown        = require("config.languages.markdown"),
     markdown_inline = {},
-    yaml            = {
-      ft = "yaml",
-      lsp = {
-        yamlls = {
-          cmd = { 'yaml-language-server', '--stdio' },
-          filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab', 'yaml.helm-values' },
-          root_markers = { '.git' },
-          settings = {
-            -- https://github.com/redhat-developer/vscode-redhat-telemetry#how-to-disable-telemetry-reporting
-            redhat = { telemetry = { enabled = false } },
-            -- formatting disabled by default in yaml-language-server; enable it
-            yaml = { format = { enable = true } },
-          },
-          on_init = function(client)
-            --- https://github.com/neovim/nvim-lspconfig/pull/4016
-            --- Since formatting is disabled by default if you check `client:supports_method('textDocument/formatting')`
-            --- during `LspAttach` it will return `false`. This hack sets the capability to `true` to facilitate
-            --- autocmd's which check this capability
-            client.server_capabilities.documentFormattingProvider = true
-          end,
-        }
-      },
-      mason = { "yaml-language-server" },
-      formatters = { "prettier" },
-    },
+    yaml            = require("config.languages.yaml"),
   }
 }
 
