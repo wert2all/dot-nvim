159cac797b6862a08712dab7c8fc111b37f56b27
Serhii Shvets <wert2all@gmail.com>
2025-10-10 01:05:55 +0300

    fix: remove mini.nvim

--- lua/config/mappings/buffer.lua
+++ lua/config/mappings/buffer.lua
@@ -1,30 +1,12 @@
 return function(prefix)
   return {
     ["n"] = {
-      ["<Leader>c"] = { "Close current buffer",
-        function()
-          local current = vim.api.nvim_get_current_buf()
-          require('mini.bufremove').delete(current)
-        end
-      },
-      [prefix .. "c"] = { "Close all buffers except current",
-        function()
-          local minibufremove = require('mini.bufremove')
-          local current = vim.api.nvim_get_current_buf()
-          local bufs = vim.fn.getbufinfo { buflisted = true }
-          for _, buf in ipairs(bufs) do
-            local bufnr = buf.bufnr
-            if bufnr ~= current then minibufremove.delete(bufnr, false) end
-          end
-        end
-      },
+      ["<Leader>c"]   = { "Close current buffer", function() Snacks.bufdelete() end },
+      [prefix .. "c"] = { "Close all buffers except current", function() Snacks.bufdelete.other() end },
       [prefix .. "C"] = { "Close all buffers",
         function()
-          local minibufremove = require('mini.bufremove')
-          local bufs = vim.fn.getbufinfo { buflisted = true }
-          for _, buf in ipairs(bufs) do
-            minibufremove.delete(buf.bufnr, false)
-          end
+          Snacks.bufdelete.other()
+          Snacks.bufdelete()
         end
       },
     },
